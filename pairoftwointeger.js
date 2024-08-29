0b71278a577c34eec36a3c7108a817a655c7c02d
Muthukumar Jayamurugan <muthukcse@gmail.com>
2024-08-29 16:39:45 +0530

    Updated q

--- pairoftwointeger.js
+++ pairoftwointeger.js
@@ -1,19 +1,14 @@
-const inputArr = [5, 6, 3, 4];
-const pairvalues = 7;
+const inputArr = [5, 2, 3, 8];
+const pairvalues = 5;
 const pair = findPair(inputArr, pairvalues);
 console.log("pair", pair);
 
 function findPair(arr, k) {
   let visited = {};
-  //   arr.forEach((element) => { //For Each won't break at any case
   for (const element of arr) {
-    console.log("k-element", element);
     if (visited[k - element]) {
-      console.log("visited", visited);
-      // k-element --> first and element second pair
       return true;
     } else {
-      console.log("visited", visited);
       visited[element] = true;
     }
   }
